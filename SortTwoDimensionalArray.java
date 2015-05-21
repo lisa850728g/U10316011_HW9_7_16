292922c3ea82e91321f77a2db5f73f5548b7924e
lisa850728g <lisa850728g@gmail.com>
2015-05-22 01:16:22 +0800

    update SortTwoDimensionalArray class
    1.建立一個方法去排序二維陣列
    在這個方法裡面，會先將第一列由小到大排序，如果有進行交換，那麼第二行的也會進行交換
    第二行交換完之後，判斷第一行如果是一樣的數字，就進行由小到大的排序
    2.在main method會呼叫這個方法，並且將已建立的二維陣列傳入
    但因為這個方法是void，所以不能夠回傳回main method

--- SortTwoDimensionalArray.java
+++ SortTwoDimensionalArray.java
@@ -11,5 +11,44 @@ public class SortTwoDimensionalArray{
 		for(row = 0 ; row <= 5 ; row++){
 			System.out.println("{" + sortArray[row][column] + "," + sortArray[row][column+1] + "}");
 		}
+		
+		sort(sortArray);
+	}
+	
+	public static void sort(int m[][]){
+		int row;
+		int column = 0;
+		boolean change = false;
+		
+		System.out.println("The two-dimensional array is sorted to : ");
+		
+		for(row = 0 ; row <= 5 ; row++){
+			for(int i = row+1 ; i <= 5 ; i++){
+				if(m[row][column] > m[i][column]){
+					int temp = m[row][column];
+					m[row][column] = m[i][column];
+					m[i][column] = temp;
+					
+					change = true;
+				}
+				
+				while(change == true){
+					int temp2 = m[row][column+1];
+					m[row][column+1] = m[i][column+1];
+					m[i][column+1] = temp2;
+					
+					change = false;
+				}
+				
+				if(m[row][column] == m[i][column] && m[row][column+1] > m[i][column+1]){
+					int temp3 = m[row][column+1];
+					m[row][column+1] = m[i][column+1];
+					m[i][column+1] = temp3;
+				}
+			}
+		}
+		
+		for(row = 0 ; row <= 5 ; row++)
+			System.out.println("{" + m[row][column] + "," + m[row][column+1] + "}");	
 	}
 }
